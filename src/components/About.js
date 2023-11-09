92644d48d81ca93bb5f78650becad3e0db3fe8be
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-11-09 13:08:55 +0300

    Destructured all props

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Links from "./Links";
 
-function About(props) 
+function About({bio, github, linkedin}) 
 {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {props.bio ? <p>{props.bio}</p> : null}
+      {bio ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
-      <Links github={props.github} linkedin={props.linkedin}/>
+      <Links github={github} linkedin={linkedin}/>
     </div>
   );
 }
